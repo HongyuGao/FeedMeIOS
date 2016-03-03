2e37def6f2a6410cf64ac5a0161483ff2a43b78e
Jun  Chen <tlnd.sky@gmail.com>
2016-03-03 16:13:39 +1100

    setup

---
+++ FeedMeIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedMeIOS
+//
+//  Created by Jun Chen on 3/03/2016.
+//  Copyright © 2016 FeedMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
