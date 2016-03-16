2f64f2ec9f11c2d4e00d9e0305ab9287d6c52cb7
Jun  Chen <tlnd.sky@gmail.com>
2016-03-16 22:26:11 +1100

    Check Point

--- FeedMeIOS/RestaurantTableViewController.swift
+++ FeedMeIOS/RestaurantTableViewController.swift
@@ -11,28 +11,110 @@ import UIKit
 class RestaurantTableViewController: UITableViewController {
     
     // MARK: Properties
+    
+    let TEXT_HOST = "http://ec2-52-27-149-51.us-west-2.compute.amazonaws.com:8080/"
+    let PICTURE_HOST = "http://ec2-52-27-149-51.us-west-2.compute.amazonaws.com:8080/"
+    
+    var restaurantID = [String]()
     var restaurants = [Restaurant]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Load the sample data.
         loadRestaurants()
+        
+        // Change the backgroud color of the tab bar:
+        // self.tabBarController?.tabBar.backgroundColor = UIColor.redColor()
+        // self.tabBarController?.tabBar.barTintColor = UIColor.redColor()
     }
     
     func loadRestaurants() {
-        let photo1 = UIImage(named: "rest1")!
-        let restaurant1 = Restaurant(name: "Caprese Salad", photo: photo1, openTime: "10:00")!
+        // Retrieve the list of all online shops' IDs:
+        var shopIDs = [String]()
+        shopIDs.append("1")
+        shopIDs.append("17")
+        shopIDs.append("18")
+        shopIDs.append("19")
+        shopIDs.append("20")
+        shopIDs.append("21")
+        shopIDs.append("22")
         
-        let photo2 = UIImage(named: "rest2")!
-        let restaurant2 = Restaurant(name: "Chicken and Potatoes", photo: photo2, openTime: "10:00")!
+        for shopID in shopIDs {
+            getShopData("http://ec2-52-27-149-51.us-west-2.compute.amazonaws.com:8080/restaurants/query/?id=" + shopID)
+        }
         
-        let photo3 = UIImage(named: "rest3")!
-        let restaurant3 = Restaurant(name: "Pasta with Meatballs", photo: photo3, openTime: "10:00")!
-        
-        restaurants += [restaurant1, restaurant2, restaurant3]
+//        getShopData
+
+//        let photo1 = UIImage(named: "rest1")!
+//        let restaurant1 = Restaurant(name: "Caprese Salad", photo: photo1, openTime: "10:00")!
+//        
+//        let photo2 = UIImage(named: "rest2")!
+//        let restaurant2 = Restaurant(name: "Chicken and Potatoes", photo: photo2, openTime: "10:00")!
+//        
+//        let photo3 = UIImage(named: "rest3")!
+//        let restaurant3 = Restaurant(name: "Pasta with Meatballs", photo: photo3, openTime: "10:00")!
+//        
+//        restaurants += [restaurant1, restaurant2, restaurant3]
     }
     
+    // MARK: Functions to retrieve data from databse.
+//    func retrieveOnlineShops(urlString: String) {
+//        // Retrieve data from database:
+//        let url = NSURL(string: urlString)
+//        
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+//            (myData, response, error) in
+//            
+//            dispatch_async(dispatch_get_main_queue(), {
+//                self.setShopInfo(myData!)
+//            })
+//        }
+//        
+//        task.resume()
+//    }
+    
+    func getShopData(urlString: String) {
+        let url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            (myData, response, error) in
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setShopInfo(myData!)
+            })
+        }
+        
+        task.resume()
+    }
+    
+    func setShopInfo(shopData: NSData) {
+        let json: NSDictionary
+        do {
+            json = try NSJSONSerialization.JSONObjectWithData(shopData, options: .AllowFragments) as! NSDictionary
+            
+            if let name = json["name"] as? String,  openTimeMorning = json["openTimeMorning"] as?String{
+                
+                let restaurant = Restaurant(name: name, photo: nil, openTime: openTimeMorning)!
+                
+                restaurants += [restaurant]
+                
+                do_table_refresh()
+            }
+        } catch _ {
+            
+        }
+        
+    }
+    
+    func do_table_refresh()
+    {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView.reloadData()
+            return
+        })
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
