998994051e604e3b1141968d7d80752fdc7e305a
Jun  Chen <tlnd.sky@gmail.com>
2016-04-03 19:41:45 +1000

    Code re-organize and add HistoryOrders UI.

---
+++ HistoryOrders.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryOrders.swift
+//  FeedMeIOS
+//
+//  Created by Jun Chen on 3/04/2016.
+//  Copyright © 2016 FeedMe. All rights reserved.
+//
+
+import Foundation
---
+++ HistoryOrdersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryOrdersTableViewCell.swift
+//  FeedMeIOS
+//
+//  Created by Jun Chen on 3/04/2016.
+//  Copyright © 2016 FeedMe. All rights reserved.
+//
+
+import UIKit
+
+class HistoryOrdersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
