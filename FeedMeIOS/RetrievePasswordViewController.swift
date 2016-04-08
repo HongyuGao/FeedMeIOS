fe7e971966150dbe3f091cac4e6f200fb982025b
Jun  Chen <tlnd.sky@gmail.com>
2016-04-08 16:30:12 +1000

    Retrieve Password UI.

---
+++ FeedMeIOS/RetrievePasswordViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RetrievePasswordViewController.swift
+//  FeedMeIOS
+//
+//  Created by Jun Chen on 7/04/2016.
+//  Copyright © 2016 FeedMe. All rights reserved.
+//
+
+import UIKit
+
+class RetrievePasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func CloseButtonClicked(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
